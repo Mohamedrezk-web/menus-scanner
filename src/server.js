a847d4926bbab4e4defd3278f4b283eef5539bfe
Mohamedrezk-web <mohamedrizkmr6@gmail.com>
2025-04-08 13:10:00 +0200

    remove connection to mongodb

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 import app from './app.js';
 import * as env from './config/dotenv.config.js';
-import { connectDB } from './config/db.js';
 
-connectDB().then(() => {
-  app.listen(env.default.PORT || 3000, () => {
-    console.log(`Server running on port ${env.default.PORT}`);
-  });
+app.listen(env.default.PORT || 3000, () => {
+  console.log(`Server running on port ${env.default.PORT}`);
 });
